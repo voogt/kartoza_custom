7b06f94cbf0fb3fd2fca2d17027ac05ce5307ceb
Juanique Voogt <juanique@kartoza.com>
2024-04-26 10:49:25 +0200

    add logic to allow one currency in shopping cart

--- kartoza_custom/public/js/currency_session.js
+++ kartoza_custom/public/js/currency_session.js
@@ -18,16 +18,32 @@ $(document).ready(function(){
         selected_value = $("input[name='currency']:checked").val();
 
         frappe.call({
-            method: "kartoza_custom.kartoza_custom.doctype.multi_currency_settings.multi_currency_settings.set_currency_cache", //dotted path to server method
+            method: "erpnext.e_commerce.shopping_cart.cart.get_cart_quotation", //dotted path to server method
             type: "GET",
-            args: {
-                "currency": selected_value
-            },
             callback: function(r) {
+                console.log(r.message.doc)
                 // code snippet
-                console.log(r.message)
-                location.reload()
+                if(r.message.doc.items.length == 0){
+                    frappe.call({
+                        method: "kartoza_custom.kartoza_custom.doctype.multi_currency_settings.multi_currency_settings.set_currency_cache", //dotted path to server method
+                        type: "GET",
+                        args: {
+                            "currency": selected_value
+                        },
+                        callback: function(r) {
+                            // code snippet
+                            console.log(r.message)
+                            location.reload()
+                        }
+                    });
+                }
+                else{
+                    frappe.throw(("Different currencies in shopping cart is not allowed"))
+                }
+                
             }
         });
+
+        
     });
 });
