82bbb3f79832f87a72f3ef074f4b3e2d883edfd4
Juanique Voogt <voogt001@gmail.com>
2024-12-02 16:12:03 +0200

    update cash flow version 15

--- kartoza_custom/kartoza_custom/report/kartoza_cash_flow/kartoza_cash_flow.js
+++ kartoza_custom/kartoza_custom/report/kartoza_cash_flow/kartoza_cash_flow.js
@@ -1,20 +1,19 @@
-frappe.require("assets/erpnext/js/financial_statements.js", function () {
-	frappe.query_reports["Kartoza Cash Flow"] = $.extend({}, erpnext.financial_statements);
+// Copyright (c) 2013, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
 
-	erpnext.utils.add_dimensions("Cash Flow", 10);
+frappe.query_reports["Kartoza Cash Flow"] = $.extend({}, erpnext.financial_statements);
 
-	// The last item in the array is the definition for Presentation Currency
-	// filter. It won't be used in cash flow for now so we pop it. Please take
-	// of this if you are working here.
-	
+erpnext.utils.add_dimensions("Kartoza Cash Flow", 10);
 
-	frappe.query_reports["Kartoza Cash Flow"]["filters"].splice(8, 1);
-	frappe.query_reports["Kartoza Cash Flow"]["filters"].splice(0, 1);
+// The last item in the array is the definition for Presentation Currency
+// filter. It won't be used in cash flow for now so we pop it. Please take
+// of this if you are working here.
 
-	frappe.query_reports["Kartoza Cash Flow"]["filters"].push({
-		fieldname: "include_default_book_entries",
-		label: __("Include Default FB Entries"),
-		fieldtype: "Check",
-		default: 1,
-	});
+frappe.query_reports["Kartoza Cash Flow"]["filters"].splice(8, 1);
+
+frappe.query_reports["Kartoza Cash Flow"]["filters"].push({
+	fieldname: "include_default_book_entries",
+	label: __("Include Default FB Entries"),
+	fieldtype: "Check",
+	default: 1,
 });
