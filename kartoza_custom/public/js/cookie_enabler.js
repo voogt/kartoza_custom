59caae20b20194e1ca7326fb9ad5fb3172ee9392
Juanique Voogt <voogt001@gmail.com>
2024-12-20 11:50:10 +0200

    update cookies

--- kartoza_custom/public/js/cookie_enabler.js
+++ kartoza_custom/public/js/cookie_enabler.js
@@ -102,3 +102,24 @@ document.addEventListener('DOMContentLoaded', function () {
         loadGTM(loadGoogleTagManagerGranted); // Ensure GTM script is loaded before calling granted function
     }
 });
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
