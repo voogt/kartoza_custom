1c54459d3447256a06a15e74b8615596f103f58b
Juanique Voogt <voogt001@gmail.com>
2025-04-07 15:45:16 +0200

    update txt generation

--- kartoza_custom/utils.py
+++ kartoza_custom/utils.py
@@ -6,6 +6,17 @@ from frappe.utils.global_search import search as default_search
 import requests
 from frappe.utils import flt
 
+def is_approx_six_or_twelve_months_apart(date1_str, date2_str):
+    date1 = datetime.strptime(date1_str, "%Y-%m-%d")
+    date2 = datetime.strptime(date2_str, "%Y-%m-%d")
+
+    delta_days = abs((date2 - date1).days)
+    months = delta_days / 30.44  # average month length
+
+    if 5.5 <= months <= 6.5:
+        return '02'
+    if 11.5 <= months <= 12.5:
+        return '03'
 
 @frappe.whitelist()
 def update_exchange_rate_and_amount():
@@ -64,7 +75,7 @@ def export_report_to_text(start_date, end_date, transaction_year):
     # Define the input and output date formats
     input_format = "%d/%m/%Y"
     input_format_b = "%Y-%m-%d"
-    output_format = "%Y%m"  # CCYYMM format
+    output_format = "%Y%m%d"  # CCYYMM format
 
     start = start_date
     end = end_date
@@ -73,11 +84,13 @@ def export_report_to_text(start_date, end_date, transaction_year):
 
     date_object_start = datetime.strptime(start, input_format_b)
     date_object_end = datetime.strptime(end, input_format_b)
+
+    recon_period = is_approx_six_or_twelve_months_apart(start, end)
     
-    certificate_num_transaction_date = datetime.strptime(certifcate_num_period, input_format).strftime(output_format)
+    certificate_num_transaction_date = datetime.strptime(end, input_format_b).strftime(output_format)
     
     #add int to end of this for employee certificate_num
-    certificate_num = f"{employer_paye_num}{certificate_num_transaction_date}VIPL000100000"
+    certificate_num = f"{recon_period}{employer_paye_num}{certificate_num_transaction_date}VIPL000100000"
 
     period_recon = datetime.strptime(end, input_format_b).strftime(output_format)
 
@@ -181,12 +194,15 @@ def export_report_to_text(start_date, end_date, transaction_year):
 
     for employee in employee_dict:
         _3010 = f"{certificate_num}{tracker}"
+        _3135 = employee["cell_number"].replace('+', '').replace(' ', '')
         
         if employee["custom_country_code"] == 'ZA':
             _3015 = "IRP5"
+            _4102 = employee['paye']
         else:
             _3015 = 'IT3(a)'
-
+            _4102 = 0
+            _3135 = f"00{_3135}"
         
         _3020 = 'A'
         _3025 = transaction_year
@@ -198,9 +214,8 @@ def export_report_to_text(start_date, end_date, transaction_year):
         _3100 = employee["tax_payroll_number"]
         _3263 = 46510
         _3125 = employee['company_email']
-        _3135 = employee["cell_number"]
-        _3136 = employee["cell_number"]
-        _3138 = employee["cell_number"]
+        _3136 = _3135
+        _3138 = _3135
         _3144 = employee["custom_unit_number"]
         _3145 = employee["custom_complex"]
         _3146 = employee["custom_street_number"]
@@ -239,9 +254,9 @@ def export_report_to_text(start_date, end_date, transaction_year):
         _3279 = "N"
         _3240 = 0
         _3288 = 1
-        _3601 = employee["gross_pay"]
-        _3699 = employee["gross_pay"]
-        _4102 = employee['paye']
+        _3601 = int(employee["gross_pay"])
+        _3699 = int(employee["gross_pay"])
+        
         _4141 = float(employee['emp_uif']) + float(employee['company_uif'])
         _4142 = employee['company_uif']
         _4149 = _4141 + float(_4102) + float(_4142)
@@ -249,12 +264,11 @@ def export_report_to_text(start_date, end_date, transaction_year):
 
         if employee["custom_employee_qualifies_for_eti"] == 1:
             _3026 = 'Y'
-            _4150 = '02'
 
             sql = f"""
                 SELECT 
                     tss.posting_date,
-                    COALESCE(tsd.amount, 0) AS `Minimum_monthly_wage`,
+                    2000 AS `Minimum_monthly_wage`,
                     SUM(COALESCE(tt.total_hours, 0)) AS `Actual_Hours_per_Month`,
                     COALESCE(tsd.amount, 0) AS `Actual_monthly_wage`,
                     COALESCE(tsd.amount, 0) AS `ETI_Remuneration`,
@@ -319,13 +333,12 @@ def export_report_to_text(start_date, end_date, transaction_year):
                 3026,_3026,
                 3601,_3601,
                 3699,_3699,
-                4102,_4102,
                 4141,_4141,
                 4142,_4142,
                 4149,_4149])
             
-            if employee["custom_employee_qualifies_for_eti"] == 1:
-                output_lines.append([4150,_4150])
+            # if employee["custom_employee_qualifies_for_eti"] == 1:
+            #     output_lines.append([4150,_4150])
             
             _4118 = 0
             _7004 = []
@@ -339,7 +352,6 @@ def export_report_to_text(start_date, end_date, transaction_year):
             # Group entries in eti_dict by month and year
             eti_grouped_by_month = defaultdict(list)
             for eti in _eti_dict:
-                print(f"ETIDICT {eti}")
                 _4118 = _4118 + eti['calculated_incentive']
                 posting_date_obj = datetime.strptime(str(eti['posting_date']), input_format_b)
                 month_year_key = (posting_date_obj.year, posting_date_obj.month)
@@ -349,7 +361,8 @@ def export_report_to_text(start_date, end_date, transaction_year):
             num_track = 0
             
             while current_date <= date_object_end:
-                _7006.append(current_date.month)
+                month_str = f"{current_date.month:02d}"  # formats with leading zero
+                _7006.append(month_str)
                 month_year_key = (current_date.year, current_date.month)
 
                 if month_year_key in eti_grouped_by_month:
@@ -412,11 +425,9 @@ def export_report_to_text(start_date, end_date, transaction_year):
                 3170,_3170,
                 3180,_3180,
                 3190,_3190,
-                3195,_3195,
                 3285,_3285,
                 3200,_3200,
                 3210,_3210,
-                3220,_3220,
                 3213,_3213,
                 3214,_3214,
                 3215,_3215,
@@ -428,7 +439,6 @@ def export_report_to_text(start_date, end_date, transaction_year):
                 3026,_3026,
                 3601,_3601,
                 3699,_3699,
-                4102,_4102,
                 4141,_4141,
                 4142,_4142,
                 4149,_4149,
@@ -436,11 +446,19 @@ def export_report_to_text(start_date, end_date, transaction_year):
             
             if employee["custom_country_code"] != 'ZA':
                 output_lines.append([4150,_4150])
+            else:
+                output_lines.append(
+                    [
+                        3195,_3195,
+                        3220,_3220,
+                        4102,_4102
+                    ]
+                )
             output_lines.append(9999)
 
         tracker = tracker + 1
 
-    _6010 = tracker + 1
+    _6010 = tracker
 
     output_lines.append([6010, _6010, 9999])
 
@@ -465,15 +483,29 @@ def export_report_to_text(start_date, end_date, transaction_year):
     if current_line:
         result.append(",".join(map(str, current_line)))
 
-    
-
     for i, item in enumerate(result):
         array = item.split(",")
-        result[i] = [val if val.isdigit() else f'"{val}"' for val in array]
-    
-    # for i, line in enumerate(result):
-    #     result[i] = ",".join(str(item) if isinstance(item, int) else f'"{item}"' for item in line)
-
+        updated_array = []
+        for j, val in enumerate(array):
+            if str(val) == "0" or str(val) == "0.0":
+                formatted_val = f"0.00"
+                updated_array.append(formatted_val)
+            elif val == "None":
+                updated_array.append(f'"{val}"')
+            elif val.isdigit():
+                if array[j - 1] == "3200" or array[j - 1] == "3210":
+                    formatted_val = f"{int(val):.4f}"
+                    if val == "6":
+                        formatted_val = f"0{formatted_val}"
+                    updated_array.append(formatted_val)
+                elif array[j - 1] == "4141" or array[j - 1] == "4142":
+                    formatted_val = f"{int(val):.2f}"
+                    updated_array.append(formatted_val)
+                else:
+                    updated_array.append(val)
+            else:
+                updated_array.append(f'"{val}"')
+        result[i] = updated_array
 
     return "\n".join([",".join(map(str, row)) for row in result])
 
