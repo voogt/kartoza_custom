407d5e932601ebc59be2deabc59add2c490b4aa8
Juanique Voogt <voogt001@gmail.com>
2025-11-10 09:33:39 +0200

    fix bug

--- kartoza_custom/hooks.py
+++ kartoza_custom/hooks.py
@@ -116,10 +116,10 @@ web_include_js = [
 # ---------------
 # Override standard doctype classes
 
-override_doctype_class = {
-	# "ToDo": "custom_app.overrides.CustomToDo"
-    "E Commerce Settings": "kartoza_custom.overrides.MultiCurrency"
-}
+# override_doctype_class = {
+# 	# "ToDo": "custom_app.overrides.CustomToDo"
+#     "E Commerce Settings": "kartoza_custom.overrides.MultiCurrency"
+# }
 
 # Document Events
 # ---------------
